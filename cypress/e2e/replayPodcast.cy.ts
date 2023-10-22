3b2bd699e0f5ed000f0f040d2beaa55fe59ee821
JesusDR01 <jesus20.11@hotmail.es>
2023-10-22 13:47:46 +0200

    fix: test

--- cypress/e2e/replayPodcast.cy.ts
+++ cypress/e2e/replayPodcast.cy.ts
@@ -12,7 +12,7 @@ describe('Replay podcast', () => {
 		cy.visit('http://localhost:3000/');
 		cy.get('[data-testid="podcast-link"]').first().click();
 		cy.get('[data-testid="play-pause"]').wait(2000);
-		cy.get('span[data-testid="play-pause"]').first().click({ force: true });
+		cy.get('span[data-testid="play-pause"]').first().click({ force: true }).wait(5000);
 		cy.get('audio').should('exist');
 		cy.get('[data-testid="duration"]').wait(2000).should('not.contain', '00:00');
 	});
