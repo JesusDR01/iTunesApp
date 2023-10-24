70710f3ae401927e3ffb6b8bb053244ec60f789c
JesusDR01 <jesus20.11@hotmail.es>
2023-10-24 17:10:20 +0200

    fix: restore tests

--- cypress/e2e/searchPodcast.cy.ts
+++ cypress/e2e/searchPodcast.cy.ts
@@ -1,18 +1,18 @@
 describe('Search podcast', () => {
-	// it('should search podcast and load the results', () => {
-	// 	cy.visit('http://localhost:3000/');
-	// 	cy.get('[role="search"] input').type('Test');
-	// 	cy.get('[data-testid="PlayArrowIcon"]').should('exist');
-	// });
+	it('should search podcast and load the results', () => {
+		cy.visit('http://localhost:3000/');
+		cy.get('[role="search"] input').type('Test');
+		cy.get('[data-testid="PlayArrowIcon"]').should('exist');
+	});
 
-	// it('should search load list and go back to top podcasts', () => {
-	// 	cy.visit('http://localhost:3000/');
-	// 	cy.get('[role="search"] input').type('Test');
-	// 	cy.get('[data-testid="PlayArrowIcon"]').wait(2000);
-	// 	cy.get('[data-testid="PlayArrowIcon"]').should('exist');
-	// 	cy.get('[role="search"] input').clear();
-	// 	cy.get('[data-testid="PlayArrowIcon"]').wait(2000).should('not.exist');
-	// });
+	it('should search load list and go back to top podcasts', () => {
+		cy.visit('http://localhost:3000/');
+		cy.get('[role="search"] input').type('Test');
+		cy.get('[data-testid="PlayArrowIcon"]').wait(2000);
+		cy.get('[data-testid="PlayArrowIcon"]').should('exist');
+		cy.get('[role="search"] input').clear();
+		cy.get('[data-testid="PlayArrowIcon"]').wait(2000).should('not.exist');
+	});
 
 	it('should load infinite elements', () => {
 		cy.visit('http://localhost:3000/');
